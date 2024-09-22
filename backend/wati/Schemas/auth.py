8e4b02beebbe6bb65689d951324b3be47765d770
ramblinghermit0403 <shivharehimansh@gmail.com>
2024-09-22 12:29:00 +0530

    Delete backend/wati directory

--- backend/wati/Schemas/auth.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class login(BaseModel):
-    email:str
-    password:str
--- backend/wati/Schemas/woocommerce.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class wocommerceConfig(BaseModel):
-    template_data:str
